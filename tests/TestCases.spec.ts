21ba274181d5e44d93d30f31a55b8a0a0855d6b2
rifkykurniawan <toettoet.tt@gmail.com>
2026-06-11 08:55:50 +0800

    Create record and run, update env and add case for login

---
+++ tests/TestCases.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect, defineConfig } from '@playwright/test';
+
+test('TC-1 - Login with valid credentials', async ({ page }) => {
+  const domain = process.env.DOMAIN!;
+  const username = process.env.USERNAMETEST!;
+  const password = process.env.PASSWORD!;
+
+  await page.goto('https://v2.hrmlabs.com');
+  await page.getByRole('textbox', { name: 'Domain' }).click();
+  await page.getByRole('textbox', { name: 'Domain' }).fill(domain);
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill(username);
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill(password);
+  await page.locator('.m-checkbox > span').click();
+  await page.getByRole('button', { name: 'Log In' }).click();
+  await page.getByRole('button', { name: 'OK' }).click();
+  await page.locator('.m-checkbox > span').click();
+  await page.getByRole('button', { name: 'Close' }).click();
+  await expect(page.locator('#companyLogoImage')).toBeVisible();
+});
