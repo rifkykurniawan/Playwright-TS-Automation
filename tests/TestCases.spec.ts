a7e1936278fd40fcc37370b269b7f2432abddf0a
rifkykurniawan <toettoet.tt@gmail.com>
2026-06-30 13:10:32 +0700

    update cases

--- tests/TestCases.spec.ts
+++ tests/TestCases.spec.ts
@@ -4,6 +4,7 @@ const domain = process.env.DOMAIN!;
 const username = process.env.USERNAMETEST!;
 const password = process.env.PASSWORD!;
 const invalidPassword = process.env.INVALIDPASSWORD!;
+const invalidUsername = process.env.INVALIDUSERNAME!;
 test('TC-1 - Login with valid credentials', async ({ page }) => {
 
   await page.goto('https://v2.hrmlabs.com');
@@ -21,7 +22,7 @@ test('TC-1 - Login with valid credentials', async ({ page }) => {
   await expect(page.locator('#companyLogoImage')).toBeVisible();
 });
 
-test('TC-2 - Login with invalid credentials', async ({ page }) => {
+test('TC-2 - Login with invalid password', async ({ page }) => {
   await page.goto('https://v2.hrmlabs.com');
   await page.getByRole('textbox', { name: 'Domain' }).click();
   await page.getByRole('textbox', { name: 'Domain' }).fill(domain);
@@ -29,6 +30,20 @@ test('TC-2 - Login with invalid credentials', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Username' }).fill(username);
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill(invalidPassword);
+  await page.locator('.m-checkbox > span').click();
+  await page.getByRole('button', { name: 'Log In' }).click();
+  await page.getByRole('button', { name: 'OK' }).click();
+  await page.getByRole('heading', { name: 'Log in to your account' }).isVisible();
+});
+
+test('TC-3 - Login with invalid username', async ({ page }) => {
+  await page.goto('https://v2.hrmlabs.com');
+  await page.getByRole('textbox', { name: 'Domain' }).click();
+  await page.getByRole('textbox', { name: 'Domain' }).fill(domain);
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill(invalidUsername);
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill(password);
   await page.locator('.m-checkbox > span').click();
   await page.getByRole('button', { name: 'Log In' }).click();
   await page.getByRole('button', { name: 'OK' }).click();
