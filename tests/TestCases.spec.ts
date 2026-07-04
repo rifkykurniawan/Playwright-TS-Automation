587f239b23a532096bcf730c62bb8d0bba33ee4a
rifkykurniawan <toettoet.tt@gmail.com>
2026-07-04 20:38:42 +0700

    update test cases

--- tests/TestCases.spec.ts
+++ tests/TestCases.spec.ts
@@ -63,3 +63,17 @@ test('TC-4 - Login with invalid username and password', async ({ page }) => {
   await page.getByRole('button', { name: 'OK' }).click();
   await page.getByRole('heading', { name: 'Log in to your account' }).isVisible();
 });
+
+test('TC-5 - Verify home page elements after successful login', async ({ page }) => {
+  await page.goto('https://v2.hrmlabs.com');
+  await page.getByRole('textbox', { name: 'Domain' }).click();
+  await page.getByRole('textbox', { name: 'Domain' }).fill(domain);
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill(username);
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill(password);
+  await page.locator('.m-checkbox > span').click();
+  await page.getByRole('button', { name: 'Log In' }).click();
+  await page.getByRole('button', { name: 'OK' }).click();
+  await expect(page.locator('#companyLogoImage')).toBeVisible();
+});
